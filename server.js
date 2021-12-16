1f4612744cae7782cdc6359e7c02d9fe22ec1708
Ernest Bruno <erndobruno@gmail.com>
2021-12-15 16:02:12 -0800

    Change userRoute to matchesRoute, user to matches.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var passport = require('passport');
 var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var matchesRouter = require('./routes/matches');
 
 var isLoggedIn = require('./config/auth.js');
 
@@ -41,7 +41,7 @@ app.use(function (req, res, next){
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/matches', matchesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
