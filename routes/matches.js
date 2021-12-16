6033f52fdc1d9692e42758831a1ae47e4fea72ba
Ernest Bruno <erndobruno@gmail.com>
2021-12-15 17:18:23 -0800

    Add a route for /matches/new

--- routes/matches.js
+++ routes/matches.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET matches listing. */
 router.get('/', function(req, res, next) {
   res.render('matches/index');
+});
+
+/* GET matches form - NEW */
+router.get('/new', function(req, res, next) {
+  res.render('matches/new');
 });
 
 module.exports = router;
