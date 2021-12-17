2b57f19c9941bcf08793bce0bcc128e2b5d5567b
Ernest Bruno <erndobruno@gmail.com>
2021-12-17 09:56:49 -0800

    Add a create function that successfully adds a note and updates the corresponding match's notes array. Sends user back to details page.

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 module.exports = {
-    new: newNote
+    new: newNote,
+    create
 }
 
 const Match = require('./../models/match');
@@ -8,3 +9,16 @@ const Note = require('./../models/note');
 function newNote(req, res){
     res.render('notes/new', {matchId: req.params.id})
 }
+
+async function create(req, res){
+    const match = await Match.findById(req.params.id);
+    req.body.postedBy = res.locals.user;
+    const newNote = new Note(req.body);
+    console.log(match, newNote);
+    newNote.save((err) => {
+        match.notes.push(newNote);
+        match.save((err)=> {
+            res.redirect(`/matches/${req.params.id}`);
+        });
+    });
+};
