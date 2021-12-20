f950160b5b93a33a2681205ea295eb569ac2f158
Ernest Bruno <erndobruno@gmail.com>
2021-12-20 09:45:46 -0800

    Update paths.

--- public/javascripts/getCharInfo.js
+++ public/javascripts/getCharInfo.js
@@ -1,75 +1,75 @@
 const characters = [
     {
         name: 'ashe',
-        path: './../../public/images/character-icons/ashe.svg',
+        path: '/images/character-icons/ashe.svg',
     },{
         name: 'bangalore',
-        path: './../../public/images/character-icons/bangalore.svg',
+        path: '/images/character-icons/bangalore.svg',
     },{
         name: 'bloodhound',
-        path: './../../public/images/character-icons/bloodhound.svg',
+        path: '/images/character-icons/bloodhound.svg',
     },{
         name: 'caustic',
-        path: './../../public/images/character-icons/caustic.svg',
+        path: '/images/character-icons/caustic.svg',
     },{
         name: 'crypto',
-        path: './../../public/images/character-icons/crypto.svg',
+        path: '/images/character-icons/crypto.svg',
     },{
 
         name: 'fuse',
-        path: './../../public/images/character-icons/fuse.svg',
+        path: '/images/character-icons/fuse.svg',
     },{
 
         name: 'gibraltar',
-        path: './../../public/images/character-icons/gibraltar.svg',
+        path: '/images/character-icons/gibraltar.svg',
     },{
 
         name: 'horizon',
-        path: './../../public/images/character-icons/horizon.svg',
+        path: '/images/character-icons/horizon.svg',
     },{
 
         name: 'lifeline',
-        path: './../../public/images/character-icons/lifeline.svg',
+        path: '/images/character-icons/lifeline.svg',
     },{
 
         name: 'loba',
-        path: './../../public/images/character-icons/loba.svg',
+        path: '/images/character-icons/loba.svg',
     },{
 
         name: 'mirage',
-        path: './../../public/images/character-icons/mirage.svg',
+        path: '/images/character-icons/mirage.svg',
     },{
 
         name: 'octane',
-        path: './../../public/images/character-icons/octane.svg',
+        path: '/images/character-icons/octane.svg',
     },{
 
         name: 'pathfinder',
-        path: './../../public/images/character-icons/pathfinder.svg',
+        path: '/images/character-icons/pathfinder.svg',
     },{
 
         name: 'rampart',
-        path: './../../public/images/character-icons/rampart.svg',
+        path: '/images/character-icons/rampart.svg',
     },{
 
         name: 'revenant',
-        path: './../../public/images/character-icons/revenant.svg',
+        path: '/images/character-icons/revenant.svg',
     },{
 
         name: 'seer',
-        path: './../../public/images/character-icons/seer.svg',
+        path: '/images/character-icons/seer.svg',
     },{
 
         name: 'valkyrie',
-        path: './../../public/images/character-icons/valkyrie.svg',
+        path: '/images/character-icons/valkyrie.svg',
     },{
 
         name: 'wattson',
-        path: './../../public/images/character-icons/wattson.svg',
+        path: '/images/character-icons/wattson.svg',
     },{
 
         name: 'wraith',
-        path: './../../public/images/character-icons/wraith.svg',
+        path: '/images/character-icons/wraith.svg',
     }
 ]
 
